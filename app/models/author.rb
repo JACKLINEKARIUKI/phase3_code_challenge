24aca8338327e8137ceb86bea7044cba178ed58b
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-10-30 08:31:46 +0300

    add initializers, readers and writers

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,25 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
-  
   end
 
+  def name
+    @name
+  end
+
+  # def articles
+  #   Article.all.select do |article|
+  #     article.author == self
+  #   end
+  # end
+
+  # def magazines
+  #   Magazine.all.uniq.select do |magazine|
+  #     magazine.author == self
+  #   end
+  # end
 
 end
